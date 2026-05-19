703419e039df5d8d9fe6701198d4965d5bf839f2
shtounek <t.sheinez@gmail.com>
2026-05-19 12:33:08 +0200

    ModSecurity done, waiting for the vault.ts file for the final binding; basic tests are OKat the moment

--- vault/scripts/init.sh
+++ vault/scripts/init.sh
@@ -7,37 +7,41 @@ export VAULT_ADDR="http://vault_server:8200"
 
 INITIALIZED=$(vault status -format=json 2>/dev/null | jq -r '.initialized' || echo "false")
 
-if [ "$INITIALIZED" = "true" ]; then
-	echo "Vault already initialized!"
-	exit 0
+if [ "$INITIALIZED" = "false" ]; then
+    echo "Vault initialization..."
+    INIT_STARTING=$(vault operator init \
+        -key-shares=1 \
+        -key-threshold=1 \
+        -format=json)
+
+    echo "Backup of unseal key and root token..."
+    echo "$INIT_STARTING" | jq -r '.unseal_keys_b64[0]' > /vault/file/unseal.key
+    echo "$INIT_STARTING" | jq -r '.root_token'          > /vault/file/root.token
+
+    echo "Unsealing..."
+    vault operator unseal "$(cat /vault/file/unseal.key)"
+
+    export VAULT_TOKEN="$(cat /vault/file/root.token)"
+
+    echo "Activation KV v2..."
+    vault secrets enable -path=secret kv-v2
+
+    DB_PASS="$(openssl rand -base64 24)"
+    JWT_SECRET="$(openssl rand -base64 48)"
+
+    echo "Injecting secrets..."
+    vault kv put -mount=secret transcendance \
+        db_password="$DB_PASS" \
+        jwt_secret="$JWT_SECRET"
+else
+    echo "Vault already initialized!"
+    echo "Retrieving existing secrets for Postgres..."
+    
+    export VAULT_TOKEN="$(cat /vault/file/root.token)"
+    
+    DB_PASS=$(vault kv get -format=json -mount=secret transcendance | jq -r '.data.data.db_password')
 fi
-
-echo "Vault initialization..."
-INIT_STARTING=$(vault operator init \
-	-key-shares=1 \
-	-key-threshold=1 \
-	-format=json)
-
-echo "Backup of unseal key and root token..."
-echo "$INIT_STARTING" | jq -r '.unseal_keys_b64[0]' > /vault/file/unseal.key
-echo "$INIT_STARTING" | jq -r '.root_token'          > /vault/file/root.token
-
-echo "Unsealing..."
-vault operator unseal "$(cat /vault/file/unseal.key)"
-
-export VAULT_TOKEN="$(cat /vault/file/root.token)"
-
-echo "Activation KV v2..."
-vault secrets enable -path=secret kv-v2
-
-DB_PASS="$(openssl rand -base64 24)"
-JWT_SECRET="$(openssl rand -base64 48)"
-
-echo "Injecting secrets..."
-vault kv put -mount=secret transcendance \
-	db_password="$DB_PASS" \
-	jwt_secret="$JWT_SECRET"
 
 echo "$DB_PASS" > /vault/file/.db_pass
 
-echo "Vault initialization completed!"
+echo "Vault initialization task completed successfully!"
